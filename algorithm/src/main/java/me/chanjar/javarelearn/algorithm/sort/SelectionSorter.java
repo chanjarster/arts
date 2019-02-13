1dc63d165303173669f00076e82d59e2d590fbb3
Daniel Qian <chanjarster@gmail.com>
2019-02-13 17:29:25 +0800

    finish #25

---
+++ algorithm/src/main/java/me/chanjar/javarelearn/algorithm/sort/SelectionSorter.java
@@ -0,0 +1,39 @@
+package me.chanjar.javarelearn.algorithm.sort;
+
+/**
+ * <a href="https://time.geekbang.org/column/article/41802">极客时间 - 数据结构与算法之美 - 11 | 排序（上）：为什么插入排序比冒泡排序更受欢迎？</a>
+ * <p>选择排序</p>
+ */
+public class SelectionSorter implements Sorter {
+
+  public int[] sort(int[] a) {
+    int n = a.length;
+    if (n <= 1) {
+      return a;
+    }
+
+    for (int i = 0; i < n; i++) {
+
+      int min_i = i;
+      // 找到最小值
+      for (int j = i + 1; j < n; j++) {
+        if (a[j] < a[min_i]) {
+          // 更新最小值数据
+          min_i = j;
+        }
+      }
+
+      if (min_i == i) {
+        continue;
+      }
+
+      // 交换数据
+      int tmp = a[i];
+      a[i] = a[min_i];
+      a[min_i] = tmp;
+
+    }
+
+    return a;
+  }
+}
---
+++ algorithm/src/test/java/me/chanjar/javarelearn/algorithm/sort/SelectionSorterTest.java
@@ -0,0 +1,9 @@
+package me.chanjar.javarelearn.algorithm.sort;
+
+public class SelectionSorterTest extends SorterTestBase {
+  @Override
+  protected Sorter getSorter() {
+    return new SelectionSorter();
+  }
+
+}
