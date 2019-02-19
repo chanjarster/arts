5765b8c7476dcc45501d52f0fa558ed79a922ebe
Daniel Qian <chanjarster@gmail.com>
2019-02-19 21:49:35 +0800

    finish #28 寻找第K大数字

---
+++ algorithm/src/main/java/me/chanjar/javarelearn/algorithm/sort/FindKthBigNumber.java
@@ -0,0 +1,46 @@
+package me.chanjar.javarelearn.algorithm.sort;
+
+/**
+ * 在一个数组中查找第K大数组
+ * <p>
+ * 题目来源：<a href="https://time.geekbang.org/column/article/41913">极客时间 - 数据结构与算法之美 - 12 | 排序（下）：如何用快排思想在O(n)内查找第K大元素？</a>
+ * </p>
+ */
+public class FindKthBigNumber {
+
+  public static int findKthBigNumber(int[] a, int kthBig) {
+
+    int n = a.length;
+    int lthSmall = n - kthBig;
+    return quickFind(a, 0, a.length - 1, lthSmall);
+  }
+
+  private static int quickFind(int[] a, int start, int end, int lthSmall) {
+
+    int pivot = a[end];
+
+    int pivot_i = start;
+    for (int j = start; j <= end - 1; j++) {
+      if (a[j] < pivot) {
+        int tmp = a[j];
+        a[j] = a[pivot_i];
+        a[pivot_i] = tmp;
+        pivot_i++;
+      }
+    }
+
+    a[end] = a[pivot_i];
+    a[pivot_i] = pivot;
+
+    if (pivot_i == lthSmall) {
+      return a[pivot_i];
+    }
+
+    if (pivot_i < lthSmall) {
+      return quickFind(a, pivot_i + 1, end, lthSmall);
+    }
+
+    return quickFind(a, start, pivot_i - 1, lthSmall);
+
+  }
+}
---
+++ algorithm/src/test/java/me/chanjar/javarelearn/algorithm/sort/FindKthBigNumberTest.java
@@ -0,0 +1,29 @@
+package me.chanjar.javarelearn.algorithm.sort;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FindKthBigNumberTest {
+
+  @Test
+  public void testFindKthBigNumber() {
+
+    assertEquals(4, FindKthBigNumber.findKthBigNumber(new int[] { 1, 2, 3, 4 }, 1));
+    assertEquals(3, FindKthBigNumber.findKthBigNumber(new int[] { 1, 2, 3, 4 }, 2));
+    assertEquals(2, FindKthBigNumber.findKthBigNumber(new int[] { 1, 2, 3, 4 }, 3));
+    assertEquals(1, FindKthBigNumber.findKthBigNumber(new int[] { 1, 2, 3, 4 }, 4));
+
+    assertEquals(4, FindKthBigNumber.findKthBigNumber(new int[] { 4, 3, 2, 1 }, 1));
+    assertEquals(3, FindKthBigNumber.findKthBigNumber(new int[] { 4, 3, 2, 1 }, 2));
+    assertEquals(2, FindKthBigNumber.findKthBigNumber(new int[] { 4, 3, 2, 1 }, 3));
+    assertEquals(1, FindKthBigNumber.findKthBigNumber(new int[] { 4, 3, 2, 1 }, 4));
+
+    assertEquals(1, FindKthBigNumber.findKthBigNumber(new int[] { 1, 1, 1, 1 }, 1));
+    assertEquals(1, FindKthBigNumber.findKthBigNumber(new int[] { 1, 1, 1, 1 }, 2));
+    assertEquals(1, FindKthBigNumber.findKthBigNumber(new int[] { 1, 1, 1, 1 }, 3));
+    assertEquals(1, FindKthBigNumber.findKthBigNumber(new int[] { 1, 1, 1, 1 }, 4));
+
+  }
+
+}
